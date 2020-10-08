406c5c208658f935b2b5e93b191913defae31825
Tiago Marques <tiagobm564@gmail.com>
2020-10-07 21:01:20 -0300

    Criação do projeto

---
+++ src/main/java/com/javatech/javatechdemo/JavaTechDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javatech.javatechdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTechDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTechDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javatech/javatechdemo/JavaTechDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatech.javatechdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTechDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
