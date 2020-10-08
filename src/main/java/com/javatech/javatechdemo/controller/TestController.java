090e4b0480f6c1dbf0df1c083e7074954494af0e
Tiago Marques <tiagobm564@gmail.com>
2020-10-07 21:32:14 -0300

    Modificado projeto_2

--- src/main/java/com/javatech/javatechdemo/controller/TestController.java
+++ src/main/java/com/javatech/javatechdemo/controller/TestController.java
@@ -1,5 +1,10 @@
 package com.javatech.javatechdemo.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class TestController {
 
 }
